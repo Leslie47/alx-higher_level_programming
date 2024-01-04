773b728a979026b5076e36634a5dcad95a3f7b87
Leslie <leslieasante44@gmail.com>
2024-01-04 13:29:25 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     argument = len(sys.argv) - 1
 
     if argument == 0:
-        print("{} arguments".format(argument))
+        print("{} arguments.".format(argument))
     elif argument == 1:
-        print("{} argument".format(argument))
+        print("{} argument:".format(argument))
     else:
-        print("{} arguments".format(argument))
+        print("{} arguments:".format(argument))
 
 
     if argument >= 1:
