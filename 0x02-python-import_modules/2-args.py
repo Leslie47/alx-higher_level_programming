440e7b6a805838e5d744a74a6a6e7e7c6616e04e
Leslie <leslieasante44@gmail.com>
2024-01-04 13:21:09 -0800

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argument = len(sys.argv) - 1
+
+    if argument == 0:
+        print("{} arguments".format(argument))
+    elif argument == 1:
+        print("{} argument".format(argument))
+    else:
+        print("{} arguments".format(argument))
+
+
+    if argument >= 1:
+        argument = 0
+        for arg in sys.argv:
+            if argument != 0:
+                print("{}: {}".format(argument, arg))
+            argument += 1
