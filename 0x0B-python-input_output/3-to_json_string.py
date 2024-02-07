12a58d11ab6bba9b4f994e2f441fba8402a4778c
Leslie <leslieasante44@gmail.com>
2024-02-06 16:57:28 -0800

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function that returns the JSON representation of an object (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Args: my_obj
+    returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
