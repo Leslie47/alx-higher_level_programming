2bb1ce02d5094ff53f36709cf4472b9311ecd745
Leslie <leslieasante44@gmail.com>
2024-02-06 16:44:17 -0800

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains function that appends to a text file and returns number
+of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at the end of a text file"""
+    with open(filename, "a", encoding="UTF-8") as f:
+        return f.write(text)
